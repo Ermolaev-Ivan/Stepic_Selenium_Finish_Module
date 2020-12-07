b364b7e50bb324d9255684917af7067da56dcd10
Ermolaev-Ivan <thedollmaster@bk.ru>
2020-12-07 13:05:18 +0300

    Created and written class in login_page Appended CONSTANT for locators written tests for login_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,20 @@
 # Для начала сделаем базовую страницу, от которой будут унаследованы все остальные классы.
 # В ней мы опишем вспомогательные методы для работы с драйвером.
+from selenium.common.exceptions import NoSuchElementException
 
-class BasePage():
-    def __init__(self, browser, url):
+
+class BasePage:
+    def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)   # неявное ожидание
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,43 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url
-    page.open() # открываем страницу
-    page.go_to_login_page() # выполняем метод страницы — переходим на страницу логин
+    page.open()  # открываем страницу
+    page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логин
 
+
+def test_guest_should_see_login_link(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer'
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_should_be_login_page(browser):  # общий тест для login_page
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
+'''тесты по одному'''
+# def test_should_register_form_login_page(browser):
+#     link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+#     page = LoginPage(browser, link)
+#     page.open()
+#     page.should_be_register_form()
+
+
+# def test_should_login_form_login_page(browser):
+#     link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+#     page = LoginPage(browser, link)
+#     page.open()
+#     page.should_be_login_form()
+#
+#
+# def test_should_be_login_url_login_page(browser):
+#     link = 'http://selenium1py.pythonanywhere.com/en-gb/accounts/login/'
+#     page = LoginPage(browser, link)
+#     page.open()
+#     page.should_be_login_url()
